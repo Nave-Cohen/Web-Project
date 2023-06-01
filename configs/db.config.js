299b2260f58237268bc53edd84274e33b107679e
nave cohen <nave.cohen@e.braude.ac.il>
2023-06-01 16:44:50 +0300

    add tests

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 const options = {
-  host: process.env.DB_HOST || process.env.GITHUB_DB_HOST,
-  user: process.env.DB_USER || process.env.GITHUB_DB_USER,
-  password: process.env.DB_PASS || process.env.GITHUB_DB_PASS,
-  database: process.env.DB_DATABASE || process.env.GITHUB_DB_DATABASE,
+  host: process.env.DB_HOST || process.env.DB_CONFIG.host,
+  user: process.env.DB_USER || process.env.DB_CONFIG.user,
+  password: process.env.DB_PASS || process.env.DB_CONFIG.password,
+  database: process.env.DB_DATABASE || process.env.DB_CONFIG.database,
 };
 
 module.exports.options = options;
