4a951bfe5488fac539be7733b301ddf0207e7917
nave cohen <nave.cohen@e.braude.ac.il>
2023-06-01 20:39:03 +0300

    db and tests

--- services/db.service.js
+++ services/db.service.js
@@ -1,86 +1,60 @@
 const mysql = require("mysql2");
+const fs = require("fs");
 const db_config = require("../configs/db.config");
 
 const pool = mysql.createPool(db_config.options).promise();
+/* create database */
 
-/* create user table if not exist */
-async function create_user_table() {
-  let connection;
-  try {
-    connection = await pool.getConnection();
-    await connection.query(`CREATE TABLE IF NOT EXISTS users (
-      id INT AUTO_INCREMENT,
-      username VARCHAR(255) NOT NULL UNIQUE,
-      email VARCHAR(255) NOT NULL UNIQUE,
-      password VARCHAR(255) NOT NULL,
-      PRIMARY KEY (id)
-    )`);
-  } catch (error) {
-    console.error("Error creating users table:", error);
-  }
-}
-
-/* create task table if not exist */
-async function create_task_table() {
-  let connection;
-  try {
-    connection = await pool.getConnection();
-    await connection.query(`CREATE TABLE IF NOT EXISTS tasks (
-      id INT AUTO_INCREMENT,
-      uid INT,
-      title VARCHAR(255),
-      content VARCHAR(255),
-      created DATE,
-      PRIMARY KEY (id),
-      FOREIGN KEY (uid) REFERENCES users(id)
-    )`);
-  } catch (error) {
-    console.error("Error creating tasks table:", error);
-  }
+function createDataBase() {
+  const sqlDump = fs.readFileSync(
+    __dirname + "/../tests/db/Todo_nodeJs_data.sql",
+    "utf-8"
+  );
+  const sqlStatement = sqlDump.split(";\n");
+  sqlStatement.forEach((statement) => {
+    statement = statement.trim();
+    if (statement !== 0 && !statement.match(/\/\*/)) {
+      pool.query(statement);
+    }
+  });
 }
 
 /* register users */
 async function register(username, email, password) {
-  let connection;
-  try {
-    connection = await pool.getConnection();
-    await connection.query(
-      "INSERT INTO users (username,email,password) VALUES (?,?,?)",
-      [username, email, password]
-    );
-    return true;
-  } catch (error) {
-    return false;
-  }
+  return await pool
+    .query("INSERT INTO users (username,email,password) VALUES (?,?,?)", [
+      username,
+      email,
+      password,
+    ])
+    .then(([rows]) => {
+      return rows.affectedRows > 0;
+    })
+    .catch((error) => {
+      return error;
+    });
 }
 
 /* login users */
 async function login(usernameOrEmail, password) {
-  let connection;
-  try {
-    connection = await pool.getConnection();
-    const [user] = await connection.query(
+  return await pool
+    .query(
       "SELECT * FROM users WHERE (username = ? OR email = ?) AND password = ?",
       [usernameOrEmail, usernameOrEmail, password]
-    );
-    if (user.length > 0) {
-      return user[0];
-    }
-    return null;
-  } catch (error) {
-    console.error("Error logging in:", error);
-    return null;
-  }
+    )
+    .then(([rows]) => {
+      return rows.length > 0 ? rows[0] : null;
+    })
+    .catch((error) => {
+      return error;
+    });
 }
 
 async function initializeDatabase() {
   try {
-    await create_user_table(); // must come before task
-    await create_task_table();
+    createDataBase();
   } catch (error) {
     console.error("Error initializing database:", error);
-  } finally {
-    await pool.end();
   }
 }
 
