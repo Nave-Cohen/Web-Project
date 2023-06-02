bb537bf3ee3c9f9c2ca5540185618ef2fe8da64d
nave cohen <nave.cohen@e.braude.ac.il>
2023-06-02 10:13:06 +0300

    npm build - load db

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 const options = {
-  host: process.env.DB_HOST || process.env.GITHUB_DB_HOST,
-  user: process.env.DB_USER || process.env.GITHUB_DB_USER,
-  password: process.env.DB_PASS || process.env.GITHUB_DB_PASS,
-  database: process.env.DB_DATABASE || process.env.GITHUB_DB_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
 };
 
 module.exports.options = options;
