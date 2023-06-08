901e1f0de8e84cfa996c36694dac3327bc266f2e
nave cohen <nave.cohen@e.braude.ac.il>
2023-06-08 13:02:40 +0300

    adding docuemnt ready

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,42 +1,41 @@
-$(".sidebar").prev().appendTo("head"); // Move <style> element to end of head
-$(".sidebar").next().appendTo("body"); // Move <script> element to end of body
+$(document).ready(() => {
+  $(".sidebar").prev().appendTo("head"); // Move <style> element to end of head
+  $(".sidebar").next().appendTo("body"); // Move <script> element to end of body
 
-let animationFrameId;
-if (!window.matchMedia("(max-width: 600px)").matches) {
   let previousWidth = $(".sidebar").outerWidth();
-  setInterval(function () {
-    const currentWidth = $(".sidebar").outerWidth();
-    if (currentWidth !== previousWidth) {
-      $(".sidebar")
-        .next()
-        .css("margin-left", currentWidth + "px");
-    }
-    previousWidth = currentWidth;
-  }, 10);
-}
+  if (!window.matchMedia("(max-width: 600px)").matches) {
+    setInterval(function () {
+      const currentWidth = $(".sidebar").outerWidth();
+      if (currentWidth !== previousWidth) {
+        $(".sidebar")
+          .next()
+          .css("margin-left", currentWidth + "px");
+      }
+      previousWidth = currentWidth;
+    }, 10);
+  } else {
+    $(".sidebar").next().css("margin-left", "50px");
+  }
 
-$("#search-input").focus(() => {
-  $(".sidebar").addClass("on-search");
-});
-$("#search-input").focusout(() => {
-  $(".sidebar").removeClass("on-search");
-});
+  $("#search-input").focus(() => {
+    $(".sidebar").addClass("on-search");
+  });
+  $("#search-input").focusout(() => {
+    $(".sidebar").removeClass("on-search");
+  });
 
-$("#menu-icon").click(function () {
-  toggleMenu();
-});
+  $("#menu-icon").click(function () {
+    $(".sidebar").toggleClass("close");
+    $("#menu-icon").toggleClass("fa-xmark fa-bars");
+  });
 
-function toggleMenu() {
-  $(".sidebar").toggleClass("close");
-  $("#menu-icon").toggleClass("fa-xmark fa-bars");
-}
-
-$("#logout-item").click(() => {
-  window.location.href = "/users/logout";
-});
-$(".list-item").each(function () {
-  $(this).click(function () {
-    const url = $(this).find("medium").text();
-    window.location.href = "/tasks/" + url;
+  $("#logout-item").click(() => {
+    window.location.href = "/users/logout";
+  });
+  $(".list-item").each(function () {
+    $(this).click(function () {
+      const url = $(this).find("medium").text();
+      window.location.href = "/tasks/" + url;
+    });
   });
 });
