29a6d29d103b496e487bd4194825b8f1bd03c7fc
nave cohen <nave.cohen@e.braude.ac.il>
2023-06-12 18:19:15 +0300

    fix cookie session

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 /* eslint-disable no-undef */
-const express = require('express');
+const express = require("express");
 const app = express();
-const users = require('./routes/users');
-const tasks = require('./routes/tasks');
-const sessions = require('./services/sessions.service');
-const jwt = require('./middlewares/login.middleware');
+const users = require("./routes/users");
+const tasks = require("./routes/tasks");
+const sessions = require("./services/sessions.service");
+const jwt = require("./middlewares/login.middleware");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.use(sessions);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/public', express.static(`${__dirname}/public`));
-app.use(sessions);
+app.use("/public", express.static(`${__dirname}/public`));
 
-app.get('/', jwt, async (req, res) => {
-  res.redirect('tasks/upcoming');
+app.get("/", jwt, async (req, res) => {
+  res.redirect("tasks/upcoming");
 });
-app.use('/users', users);
+app.use("/users", users);
 
-app.use('/tasks', jwt, tasks);
+app.use("/tasks", jwt, tasks);
 
 app.use((req, res) => {
-  res.render('ejs/404.ejs');
+  res.render("ejs/404.ejs");
 });
 
-if (process.env.npm_lifecycle_event != 'test') {
+if (process.env.npm_lifecycle_event != "test") {
   app.listen(8080, () => {
-    console.log('listening on port 8080');
+    console.log("listening on port 8080");
   });
 }
 module.exports.app = app;
