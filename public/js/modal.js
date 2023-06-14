5c00169a3f2bab2ce925926a0a884847bb2b8b41
nave cohen <nave.cohen@e.braude.ac.il>
2023-06-14 18:39:43 +0300

    modal refactor

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,31 +1,44 @@
-var id;
-const $title = $("#task-title");
-const $content = $("#task-content");
-const $created = $("#task-created");
-const $datetimepicker = $("#datetimepicker");
-const $modal = $("#Modal");
-var minDateTime;
+/* eslint-disable no-undef */
+let id;
+const $title = $('#task-title');
+const $content = $('#task-content');
+const $created = $('#task-created');
+const $datetimepicker = $('#datetimepicker');
+const $modal = $('#Modal');
+let minDateTime;
+
+function updateTask(task) {
+  try {
+    $.post('/tasks/update', task, function (task) {
+      $(`.accordion-item-${task.id}`).remove();
+    });
+    $modal.hide();
+  } catch {
+    console.log('modal update-item error');
+  }
+}
+function addTask(task) {
+  try {
+    $.post('/tasks/add', task, function () {
+      location.reload();
+      // TODO: $(`.accordion`).prepend(); add new task to accordion
+    });
+    $modal.hide();
+  } catch {
+    console.log('modal update-item error');
+  }
+}
+
 // eslint-disable-next-line no-unused-vars
-function showModal(
-  _id = -1,
-  title = "",
-  content = "",
-  start = "",
-  created = ""
-) {
+function showModal(_id = -1, title = '', content = '', start = '', created = '') {
   id = _id;
   $title.val(title);
   $content.text(content);
-  // eslint-disable-next-line no-undef
-  minDateTime = moment().tz("Asia/Jerusalem").format("YYYY-MM-DDTHH:mm");
-  $datetimepicker.attr("min", minDateTime);
+  minDateTime = moment().tz('Asia/Jerusalem').format('YYYY-MM-DDTHH:mm');
+  $datetimepicker.attr('min', minDateTime);
   $datetimepicker.val(start);
-  if (id !== -1) {
-    $created.text("created - " + created);
-  } else {
-    $created.text(created);
-  }
-  $modal.data("id", id);
+  $created.text(id !== -1 ? 'created - ' + created : created);
+  $modal.data('id', id);
   $modal
     .modal({
       backdrop: false,
@@ -33,45 +46,31 @@ function showModal(
     })
     .show();
 }
-$datetimepicker.on("input", function () {
-  var selectedDateTime = new Date($(this).val());
-  var minDateTimeObj = new Date(minDateTime);
+
+$datetimepicker.on('input', function () {
+  const selectedDateTime = new Date($(this).val());
+  const minDateTimeObj = new Date(minDateTime);
 
   if (selectedDateTime < minDateTimeObj) {
     $(this).val(minDateTime);
-    alert("the must be greater then current time");
   }
 });
 
-$("#close-modal-btn").click(function () {
-  $("#Modal").hide();
+$('#close-modal-btn').click(function () {
+  $modal.hide();
 });
 
-$("#sbmt-btn").click(function () {
+$('#sbmt-btn').click(function () {
   const task = {
-    id: id,
+    id,
     title: $title.val(),
     content: $content.text(),
     start: $datetimepicker.val(),
   };
+
   if (id !== -1) {
-    try {
-      $.post("/tasks/update", task, function (task) {
-        $(`.accordion-item-${task.id}`).remove();
-      });
-      $modal.hide();
-    } catch {
-      console.log("modal update-item error");
-    }
+    updateTask(task);
   } else {
-    try {
-      $.post("/tasks/add", task, function () {
-        location.reload();
-        // TODO: $(`.accordion`).prepend(); add new task to accordion
-      });
-      $modal.hide();
-    } catch {
-      console.log("modal update-item error");
-    }
+    addTask(task);
   }
 });
