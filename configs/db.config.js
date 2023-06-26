96ef3f3b4ea5cb6f1db43eddd1afa83d3e6ee594
nave cohen <nave.cohen@e.braude.ac.il>
2023-06-26 13:25:17 +0300

    connection db 4

--- configs/db.config.js
+++ configs/db.config.js
@@ -6,8 +6,8 @@ const options = {
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  connectionLimit: 3,
-  maxIdle: 3,
+  connectionLimit: 4,
+  maxIdle: 4,
 };
 
 module.exports.options = options;
