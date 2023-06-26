a1b7a456547c21c2a96ec5d6a9f9b639dc42830a
nave cohen <nave.cohen@e.braude.ac.il>
2023-06-26 14:35:20 +0300

    Revert "connection db 4"
    This reverts commit 96ef3f3b4ea5cb6f1db43eddd1afa83d3e6ee594.

--- configs/db.config.js
+++ configs/db.config.js
@@ -6,8 +6,8 @@ const options = {
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  connectionLimit: 4,
-  maxIdle: 4,
+  connectionLimit: 3,
+  maxIdle: 3,
 };
 
 module.exports.options = options;
